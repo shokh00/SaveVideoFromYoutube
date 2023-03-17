9f30f7e89d6e39fad4d7c155abba6609252493a6
shokh00 <bamygamers@gmail.com>
2023-03-17 22:36:51 +0500

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { Component } from 'react';
-import { Input, Select, Button } from 'antd';
+import React, { Component } from 'react'
 import axios from 'axios';
+import { ToastContainer, toast, error } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Route, Routes } from 'react-router-dom';
+import { Button, Input } from 'antd';
+
 
 class App extends Component {
   state = {
@@ -10,10 +15,19 @@ class App extends Component {
     loader: false
   };
 
+  componentDidUpdate() {
+    const h2 = document.getElementsByTagName("h2")[0];
+    if (h2) {
+      document.title = h2.innerHTML
+    }
+
+    console.log(h2);
+  }
+
   changingValue = (event) => {
     let value = event.target.value;
-    let fullValue = value.split("=").at(-1);
-    this.setState({url: fullValue});
+    let fullValue = value.split(/[=/]+/).at(-1);
+    this.setState({ url: fullValue });
   }
 
   getReaquestFromBackend = () => {
@@ -22,39 +36,47 @@ class App extends Component {
       console.log(res.data)
       this.setState({ video: res.data })
     }).catch(err => {
-      console.log(err)
+      toast.error("Error");
+      console.log(err);
     }).finally(() => this.setState({ loader: false }))
   }
+
 
   render() {
     return (
       <div>
-        <h2 className='text-center'> Youtube video and mp3 downloader </h2>
+        <h1 className='text-center my-2'> Youtube video and mp3 downloader </h1>
         <div className='row m-0'>
-          <div className='col-6 offset-3 mt-2'>
-            <div className='row m-0'>
-              <div className='col-9'>
+          <div className='mt-2'>
+            <div className='row m-0 total'>
+              <div className='forinput'>
                 <Input onChange={(e) => this.changingValue(e)} placeholder='Video url' />
               </div>
-              <div className='col-3'>
+              <div className='forbtn'>
                 <Button
-                  block
                   type='primary'
                   loading={this.state.loader}
                   onClick={this.getReaquestFromBackend}
                 >
                   Search
                 </Button>
+                <ToastContainer position="top-center"
+                  autoClose={3000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="dark" />
               </div>
             </div>
           </div>
+          <div dangerouslySetInnerHTML={{ __html: this.state.video }} />
         </div>
-
-        <div dangerouslySetInnerHTML={{ __html: this.state.video }} />
-      </div>
+      </div >
     );
   }
 }
-
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import ReactDOM from "react-dom/client";
 import MyApp from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<MyApp/>)
+root.render(
+  <BrowserRouter>
+    <MyApp />
+  </BrowserRouter>
+)
 
